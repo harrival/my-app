f10166492c678b2fbe5109fa0c5b5521e490e423
harrival <harrival56@gmail.com>
2026-07-18 12:38:45 -0700

    harmonize 3

--- api/db.js
+++ api/db.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV === "test") {
 }
 
 const db = new Pool({
-  connectionString: DATABASE_URL,
+  connectionString: DB_URI,
   ...(process.env.DATABASE_URL && {
     ssl: { rejectUnauthorized: false }
   })
