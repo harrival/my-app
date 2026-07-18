d69b488be82dba001d6d7262f3d54eaca32ae69b
harrival <harrival56@gmail.com>
2026-07-18 15:03:18 -0700

    commented

--- api/appServer.js
+++ api/appServer.js
@@ -1,50 +1,50 @@
-const express = require("express");
-const ExpressError = require("./expressError");
-const middleware = require("./middleware")
-const cors = require('cors');
-// const morgan = require("morgan")
+// const express = require("express");
+// const ExpressError = require("./expressError");
+// const middleware = require("./middleware")
+// const cors = require('cors');
+// // const morgan = require("morgan")
 
-const userRoutes = require("./userRoutes")
+// const userRoutes = require("./userRoutes")
 
-const app = express();
-// Middleware to enable CORS (for development)
-app.use(cors());
-app.use(express.json());
-app.use(middleware.logger)
-// app.use(morgan('dev'))
+// const app = express();
+// // Middleware to enable CORS (for development)
+// app.use(cors());
+// app.use(express.json());
+// app.use(middleware.logger)
+// // app.use(morgan('dev'))
 
 
-app.use('/players', userRoutes)
-app.get('/favicon.ico', (req, res) => res.sendStatus(204))
+// app.use('/players', userRoutes)
+// app.get('/favicon.ico', (req, res) => res.sendStatus(204))
 
-app.get('/secret', middleware.checkForPassword, (req, res, next) => {
-  return res.send("I LOVE YOU <3 FOR REAL MARRY ME")
-})
+// app.get('/secret', middleware.checkForPassword, (req, res, next) => {
+//   return res.send("I LOVE YOU <3 FOR REAL MARRY ME")
+// })
 
-app.get('/private', middleware.checkForPassword, (req, res, next) => {
-  return res.send("YOU HAVE REACHED THE PRIVATE PAGE.  IT IS PRIVATE.")
-})
+// app.get('/private', middleware.checkForPassword, (req, res, next) => {
+//   return res.send("YOU HAVE REACHED THE PRIVATE PAGE.  IT IS PRIVATE.")
+// })
 
-// 404 handler
-app.use(function (req, res, next) {
-  return next(new ExpressError("Not Found", 404));
-});
+// // 404 handler
+// app.use(function (req, res, next) {
+//   return next(new ExpressError("Not Found", 404));
+// });
 
-// generic error handler
-app.use(function (err, req, res, next) {
-  // the default status is 500 Internal Server Error
-  let status = err.status || 500;
+// // generic error handler
+// app.use(function (err, req, res, next) {
+//   // the default status is 500 Internal Server Error
+//   let status = err.status || 500;
 
-  // set the status and alert the user
-  return res.status(status).json({
-    error: {
-      message: err.message,
-      status: status
-    }
-  });
-});
+//   // set the status and alert the user
+//   return res.status(status).json({
+//     error: {
+//       message: err.message,
+//       status: status
+//     }
+//   });
+// });
 
-const PORT = 5001;
-app.listen(PORT, function () {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// const PORT = 5001;
+// app.listen(PORT, function () {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
