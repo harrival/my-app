30682c704e1fe2de039601f077c254277a44e8ce
harrival <harrival56@gmail.com>
2026-07-18 13:04:58 -0700

    harmonize 4

--- client/src/shared/Utils/apiConfig.ts
+++ client/src/shared/Utils/apiConfig.ts
@@ -1,5 +1,5 @@
 declare var process: { env: { [key: string]: string | undefined } };
 
-const BASE_URL: string = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001';
+const BASE_URL: string = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 export { BASE_URL };
